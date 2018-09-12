17fa7f9246ef79ab8752fc67f9aa17a2d0ebf97b
李继红 <707230595@qq.com>
2018-09-12 22:14:22 +0800

    feat: 增加Alert

---
+++ src/components/Alert/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+
+export interface AlertProps {
+    show?
+    img?
+    remind?
+    doClose?
+    doConfirm?
+}
+
+export default class Alert extends React.Component<AlertProps, any> {
+    doClose() {
+        let { doClose } = this.props
+        doClose && doClose()
+    }
+    doConfirm() {
+        let { doConfirm } = this.props
+        doConfirm && doConfirm()
+    }
+    public render() {
+        let { show, img, remind } = this.props
+        return (
+            <div style={{ display: show ? 'block' : 'none' }}>
+                <div className="Alert__backdrop" />
+                <div className="Alert__remind-wrap">
+                    <div className="Alert__content-wrap">
+                        <div className="Alert__close-alert" onClick={this.doClose.bind(this)}>
+                            <i className="k-i-close-o" />
+                        </div>
+                        <div className="Alert__success-pic">
+                            <img
+                                src={img}
+                                className="Alert__success-image"
+                            />
+                        </div>
+                        <div className="Alert__remind-text">
+                            <pre className="Alert__remind">{remind}</pre>
+                        </div>
+                        <button
+                            className="Alert__back"
+                            style={{
+                                backgroundColor: 'rgb(57, 150, 246)',
+                                color: 'rgb(255, 255, 255)'
+                            }}
+                            onClick={this.doConfirm.bind(this)}
+                        >
+                            确认
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
