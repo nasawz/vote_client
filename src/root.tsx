07fa4673cb6d63bde54325dbf5ca5188a0a451d3
nasawz <nasa127@gmail.com>
2018-09-13 23:07:37 +0800

    增加获取项目列表的列子

--- src/root.tsx
+++ src/root.tsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Route, HashRouter as Router } from 'react-router-dom';
 import * as DocumentTitle from 'react-document-title';
 
+import { IQueryItemsParams } from './models/vote';
+
 import App from './app';
 export interface RootProps {
   activity;
@@ -11,6 +13,7 @@ export interface RootProps {
   getUserAsync;
   setCategory;
   getMyVoteItemAsync;
+  // getVoteItemsAsync;
 }
 
 class Root extends React.Component<RootProps, any> {
@@ -26,6 +29,17 @@ class Root extends React.Component<RootProps, any> {
       this.props.setCategory(categorys[0]);
     }
     this.props.getMyVoteItemAsync(activityId);
+    // let queryParams: IQueryItemsParams = {
+    //   activityId: activityId,
+    //   limit: 10,
+    //   skip: 0,
+    //   order: {
+    //     key: 'createAt',
+    //     type: 'desc'
+    //   },
+    //   category: categorys[0]
+    // };
+    // this.props.getVoteItemsAsync(queryParams);
   }
   public render() {
     let { activity, user } = this.props;
@@ -52,12 +66,17 @@ const mapState2Props = (state) => {
 const mapDispatch2Props = ({
   activity: { getDataAsync },
   user: { getUserAsync },
-  vote: { setCategory, getMyVoteItemAsync }
+  vote: {
+    setCategory,
+    getMyVoteItemAsync
+    // getVoteItemsAsync
+  }
 }) => ({
   getActivityData: getDataAsync,
   getUserAsync: getUserAsync,
   setCategory: setCategory,
   getMyVoteItemAsync: getMyVoteItemAsync
+  // getVoteItemsAsync: getVoteItemsAsync
 });
 
 export default connect(
