6b8325ea2634561d62cc6eeea25dae326b9c565c
nasawz <nasa127@gmail.com>
2018-09-14 11:06:14 +0800

    me 页面 色调调整

--- src/modules/vote/pages/me.tsx
+++ src/modules/vote/pages/me.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-export interface MeProps { }
+export interface MeProps {
+  primary_color;
+}
 
 class Me extends React.Component<MeProps, any> {
   public render() {
+    let { primary_color } = this.props;
     return (
       <div>
         <div className="MySignUp__wrap">
@@ -14,14 +17,11 @@ class Me extends React.Component<MeProps, any> {
               <div className="signUpInformation__detail">
                 <div className="signUpInformation__info-pic">
                   <img
-                    src="https://pic.kuaizhan.com/g3/b6/e4/6102-cb9d-4712-9e8d-1bbf6ac1f99d55"
+                    src="http://voteres.baleina.cn/cover12.jpg-75"
                     className="signUpInformation__pic"
                   />
                   <span className="signUpInformation__pic-num">3</span>
-                  <img
-                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAAAflBMVEX///////////////////////////9MaXH///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////97HwtyAAAAKXRSTlNgPiS6V7P9AMDsqynQARXyZvGZsbk3iw0CKAN8LeZtXWnfX2hbIFIdTyejEZ0AAAB4SURBVBjTfdHJDoJAEEXRYmooZ1FwFifQ+/8/6IIYSDXylmfRldyWWDHTWDQRs0QFSc2EETwGBw/3W1hZDIGZxd0SJt6bwSZf/Lk+nftYrEsPzwqnPt6qNLwAXDt84Z73tsbjhx/XJXJNi+8o6y2qR4NYHIw89B1fmZEXxD5HYPcAAAAASUVORK5CYII="
-                    className="signUpInformation__icon"
-                  />
+                  <img src="assets/images/pic.png" className="signUpInformation__icon" />
                 </div>
                 <div className="signUpInformation__information">
                   <p className="signUpInformation__name">名称: sfdasdf</p>
@@ -36,13 +36,13 @@ class Me extends React.Component<MeProps, any> {
           <div className="MySignUp__bottom-wrap">
             <div
               className="MySignUp__subcontent"
-              style={{ backgroundColor: 'rgb(57, 150, 246)', color: 'rgb(255, 255, 255)' }}
+              style={{ backgroundColor: 'red', color: 'rgb(255, 255, 255)' }}
             >
               删除作品
             </div>
             <div
               className="MySignUp__subcontent"
-              style={{ backgroundColor: 'rgb(57, 150, 246)', color: 'rgb(255, 255, 255)' }}
+              style={{ backgroundColor: primary_color, color: 'rgb(255, 255, 255)' }}
             >
               返回投票
             </div>
@@ -54,7 +54,9 @@ class Me extends React.Component<MeProps, any> {
 }
 
 const mapState2Props = (state) => {
-  return {};
+  return {
+    primary_color: state.activity.primary_color
+  };
 };
 
 export default connect(mapState2Props)(Me);
