6b22dd328ad86cf68ac74332f81984320ef1343a
nasawz <nasa127@gmail.com>
2018-09-17 15:13:40 +0800

    fix: 排行榜上移

--- src/components/RankBtn/index.tsx
+++ src/components/RankBtn/index.tsx
@@ -8,7 +8,7 @@ export default class RankBtn extends React.Component<RankBtnProps, any> {
   public render() {
     let { hander_rank_btn } = this.props;
     return (
-      <ul className="Header__to-ranking" onClick={hander_rank_btn}>
+      <ul className="Header__card Header__to-ranking" onClick={hander_rank_btn}>
         <img src="/assets/images/cup.png" className="Header__cup-pic" />
         查看榜单
       </ul>
